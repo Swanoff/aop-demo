6dc155dec301d45cb3eeb7835573a9bd94022f7a
Surya <suryathegreat.07@gmail.com>
2020-04-14 12:14:23 +0530

    AOP Pointcut Expression - Matching method with a specific parameter
    Matching method starting with add and with Account object as parameter (fully qualified class name should be given for the class name if the parameter is an object)
    
    (execution(modifier returnType methodName(parameter)))

--- src/main/java/com/swanoff/aopdemo/dao/AccountDAO.java
+++ src/main/java/com/swanoff/aopdemo/dao/AccountDAO.java
@@ -2,10 +2,12 @@ package com.swanoff.aopdemo.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.swanoff.aopdemo.Account;
+
 @Component
 public class AccountDAO {
 
-	public void addAccount() {
+	public void addAccount(Account theAccount) {
 		System.out.println(getClass() + ": Doing my db work: Adding an account");
 	}
 }
